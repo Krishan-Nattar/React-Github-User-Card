e3ce05cea4b02cf5200c09b44a8bdb2e8a1a8aba
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-03 12:29:55 -0700

    Fixed foreach loop. Added styling

--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import UserCard from "./UserCard";
-import Search from './Search';
+import Search from "./Search";
 
 class GitHub extends Component {
   constructor() {
@@ -21,39 +21,66 @@ class GitHub extends Component {
     axios
       .get("https://api.github.com/users/krishan-nattar/followers")
       .then(res => {
-        const followers = res.data;
-        this.setState({ followers: followers });
+        let followers = res.data;
+
+        followers.forEach(person => {
+          axios
+            .get(`https://api.github.com/users/${person.login}`)
+            .then(res => {
+              this.setState(prevState => {
+                const list = [...prevState.followers, res.data];
+                return { followers: list };
+              });
+            });
+        });
       });
   }
 
+  handleChange = event => {
+    this.setState({ search: event.target.value });
+  };
 
-handleChange = (event) =>{
-
-this.setState({search: event.target.value})
-}
-handleSearch = (event)=>{
+  handleSearch = event => {
     event.preventDefault();
-    axios.get(`https://api.github.com/users/${this.state.search}`).then(res => {
-        this.setState({ data: [res.data] });
-      });
-  
+
+    this.setState({ followers: [] }, () => {
+      axios
+        .get(`https://api.github.com/users/${this.state.search}`)
+        .then(res => {
+          this.setState({ data: [res.data] });
+        });
+
       axios
         .get(`https://api.github.com/users/${this.state.search}/followers`)
         .then(res => {
-          const followers = res.data;
-          this.setState({ followers: followers });
+          let followers = res.data;
+
+          followers.forEach(person => {
+            axios
+              .get(`https://api.github.com/users/${person.login}`)
+              .then(res => {
+                this.setState(prevState => {
+                  const list = [...prevState.followers, res.data];
+                  return { followers: list };
+                });
+              });
+          });
         });
-}
+    });
+  };
   render() {
     return (
       <div>
-        <Search handleChange={this.handleChange} handleSearch={this.handleSearch}/>
+        <Search
+          handleChange={this.handleChange}
+          handleSearch={this.handleSearch}
+        />
         {this.state.data
-          ? this.state.data.map(user => <UserCard user={this.state.data[0]} />)
+          ? this.state.data.map(user => <UserCard user={user} key={user.login} />)
           : null}
         {this.state.followers
           ? this.state.followers.map(person => {
-              return <UserCard user={person} />;
+              return <UserCard user={person} key={person.login}/>;
             })
           : null}
       </div>
@@ -62,4 +89,3 @@ handleSearch = (event)=>{
 }
 
 export default GitHub;
-
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { Segment } from "semantic-ui-react";
 
 const UserCard = props => {
-
   const user = props.user;
-
   return (
     <div>
-      <div>{user.login}</div>
-      <img src={user.avatar_url} className="image"/>
-      <div>{user.location}</div>
-      <img src={`http://ghchart.rshah.org/${user.login}`} />
+      <Segment raised className="segments">
+        <div className="flex">
+          <div className="avatar">
+            <img src={user.avatar_url} className="image" />
+            <div>{user.name}</div>
+            <div>{user.location}</div>
+          </div>
+          <div>
+            <div>
+              <a href={user.html_url}>View GitHub</a>
+            </div>
+            {(user.blog) ? <a href={user.blog}>View Website</a> :null}
+          </div>
+        </div>
+        <img src={`http://ghchart.rshah.org/${user.login}`} />
+      </Segment>
     </div>
   );
 };
