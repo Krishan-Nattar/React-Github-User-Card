13d2f9ae8f4d8687f1125e37082aa32c7753d52f
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-02 19:40:10 -0700

    refactor

--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -1,57 +1,73 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import UserCard from './UserCard';
+import React, { Component } from "react";
+import axios from "axios";
+import UserCard from "./UserCard";
 
 class GitHub extends Component {
-    constructor(){
-        super();
-        this.state = {
-            data: [],
-            followers: []
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      followers: []
+    };
+  }
 
-    componentDidMount(){
-        axios.get('https://api.github.com/users/krishan-nattar')
-        .then(res=>{
-            // console.log(res.data);
-            this.setState({data: [res.data]});
+  componentDidMount() {
+    axios.get("https://api.github.com/users/krishan-nattar").then(res => {
+      this.setState({ data: [res.data] });
 
-            axios.get('https://api.github.com/users/krishan-nattar/followers')
-                .then(res=>{
-                // console.log(res.data);
-                const followers = res.data
-                const followerArray = [];
+      axios
+        .get("https://api.github.com/users/krishan-nattar/followers")
+        .then(res => {
+          const followers = res.data;
+          const followerArray = [];
 
-                followers.forEach(person=>{
-                    axios.get(`https://api.github.com/users/${person.login}`)
-                    .then(res=>{
-                        // console.log(res.data);
-                        followerArray.push(res.data);
-                    });
-                });
-                console.log(followerArray);
-                this.setState({followers: followerArray});
-                });
+          followers.forEach(person => {
+            axios
+              .get(`https://api.github.com/users/${person.login}`)
+              .then(res => {
+                followerArray.push(res.data);
+              });
+          });
+          console.log(followerArray);
+          this.setState({ followers: followerArray });
         });
-    }
-    // state = {  }
-    render() { 
-        return ( 
-            <div>
-                GitHub Component
-                
-                {(this.state.data[0]) ? <UserCard data={this.state.data[0]}/> : null}
-                {/* {(this.state.followers) ? this.state.followers.map(person=>{ */}
-                {/* return <UserCard data={person}/> */}
-                {/* return <h1>hi</h1> */}
-                {/* }) : null} */}
-                {this.state.followers.map(person=>{
-                    return <h1>Hello!</h1>
-                })}
-                </div>
-         );
-    }
+    });
+  }
+  render() {
+    console.log(this.state.followers);
+
+    this.state.followers.forEach(item => {
+      console.log(item);
+    });
+    return (
+      <div>
+        GitHub Component
+        {this.state.followers.map(person => {
+          return <h1>Hello!</h1>;
+        })}
+      </div>
+    );
+  }
 }
- 
+
 export default GitHub;
+
+{
+  /*                 
+                {(this.state.data[0]) ? <UserCard data={this.state.data[0]}/> : null}
+                {this.state.data.map(thing=>{
+                    return <h1>Hello2</h1>
+                })} */
+}
+{
+  /* {(this.state.followers) ? this.state.followers.map(person=>{ */
+}
+{
+  /* return <UserCard data={person}/> */
+}
+{
+  /* return <h1>hi</h1> */
+}
+{
+  /* }) : null} */
+}
